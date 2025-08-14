dc260774a9bfd656e2151af456e7831e7d454b9f
Seow Zhen Jun <56989187+seowzhenjun0126@users.noreply.github.com>
2025-08-14 15:35:22 +0800

    Attempt to fix "No LID for user"

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -535,6 +535,9 @@ exports.LoadUtils = () => {
     window.WWebJS.getChat = async (chatId, { getAsModel = true } = {}) => {
         const isChannel = /@\w*newsletter\b/.test(chatId);
         const chatWid = window.Store.WidFactory.createWid(chatId);
+        if (!(await window.Store.QueryExist(wid))?.wid) {
+            return null;
+        }
         let chat;
 
         if (isChannel) {
